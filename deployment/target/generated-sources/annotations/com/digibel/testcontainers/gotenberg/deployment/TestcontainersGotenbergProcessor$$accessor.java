258189d9f22faaa9e2dce967d5a99852bea047f9
Beni Katznelson <beni@katznelson.ca>
2026-01-14 13:40:13 -0500

    removed target from git history

--- deployment/target/generated-sources/annotations/com/digibel/testcontainers/gotenberg/deployment/TestcontainersGotenbergProcessor$$accessor.java
+++
@@ -1,8 +0,0 @@
-package com.digibel.testcontainers.gotenberg.deployment;
-@io.quarkus.Generated("Quarkus annotation processor")
-public final class TestcontainersGotenbergProcessor$$accessor {
-    private TestcontainersGotenbergProcessor$$accessor() {}
-    public static Object construct() {
-        return new TestcontainersGotenbergProcessor();
-    }
-}
